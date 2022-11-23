dac4eeacddb7c1e1881a92a93c1b2c1301aed7b3
Rayhene <rrda@poli.br>
2022-11-23 18:25:33 -0300

    insert form in login page

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function myFunction() {
-    var areaHtml = document.querySelector('html');
-    areaHtml.classList.toggle('dark-mode')
- }
+  var areaHtml = document.querySelector("html");
+  areaHtml.classList.toggle("dark-mode");
+}
+
+function getFormData() {
+  const myForm = document.getElementById("myForm");
+  const formData = new FormData(myForm);
+  const data = Object.fromEntries(formData);
+  console.log("[FORM_DATA]", data);
+}
